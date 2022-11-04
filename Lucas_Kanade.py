e9a19b8afb8f6078a13c242bdc936801ab9a59a5
Apeksha18M <67169238+Apeksha18M@users.noreply.github.com>
2022-11-04 19:02:52 +0530

    Update Lucas_Kanade.py

--- Lucas_Kanade.py
+++ Lucas_Kanade.py
@@ -3,26 +3,28 @@ import cv2 as cv
 
 cap = cv.VideoCapture('Apporv_bottle_tracking.mp4')
 
-# params for ShiTomasi corner detection
-feature_params = dict(maxCorners=200,
+# ShiTomasi corner detection
+feature_params = dict(maxCorners=100,
                       qualityLevel=0.4,
-                      minDistance=7,
-                      blockSize=7)
+                      minDistance=7, blockSize=7)
 
 # Parameters for lucas kanade optical flow
 lk_params = dict(winSize=(15, 15),
                  maxLevel=2,
                  criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# Create some random colors
+# print(lk_params)
+
+# Creating random colors
 color = np.random.randint(0, 255, (100, 3))
 
-# Take first frame and find corners in it
+# Taking first frame and find corners in it
 ret, old_frame = cap.read()
 old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 p0 = cv.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+# print(p0)
 
-# Create a mask image for drawing purposes
+# Creating a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
 while(1):
@@ -33,16 +35,24 @@ while(1):
 
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    # calculate optical flow
+    # calculating optical flow
     p1, st, err = cv.calcOpticalFlowPyrLK(
         old_gray, frame_gray, p0, None, **lk_params)
+
+    """
+    p1: output vector of 2D points containing the calculated new positions of input features in the second image
+    st: output status vector (of unsigned chars); each element of the vector is set to 1 if the flow for the corresponding features has been found, otherwise, it is set to 0.
+    err: output vector of errors; each element of the vector is set to an error for the corresponding feature, type of the error measure can be set in flags parameter
+    """
+    # print(p1)
+    # print("...")
 
     # Select good points
     if p1 is not None:
         good_new = p1[st == 1]
         good_old = p0[st == 1]
 
-    # draw the tracks
+    # drawing the tracks
     for i, (new, old) in enumerate(zip(good_new, good_old)):
         a, b = new.ravel()
         c, d = old.ravel()
@@ -56,7 +66,7 @@ while(1):
     if k == 27:
         break
 
-    # Now update the previous frame and previous points
+    # Updating the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1, 1, 2)
 
